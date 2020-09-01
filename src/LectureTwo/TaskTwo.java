0e57b8242a1a25686ef0410e19a57211f1ea511b
comvira <60033796+comvira@users.noreply.github.com>
2020-09-01 13:55:51 +0300

    Ввести целое трехзначное число в консоли и найти сумму цифр этого трехзначного числа

--- src/LectureTwo/TaskTwo.java
+++ src/LectureTwo/TaskTwo.java
@@ -1,7 +1,11 @@
 package LectureTwo;
 
+import java.util.Scanner;
+
 public class TaskTwo {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(n / 100 + n / 10 % 10 + n % 10);
     }
 }
