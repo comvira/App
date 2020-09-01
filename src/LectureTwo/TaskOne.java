23d5ad7b41cf1a22b493b6f7f58e5c40d26167d6
comvira <60033796+comvira@users.noreply.github.com>
2020-09-01 12:24:40 +0300

    Ввести целое число в консоли и вывести на экран последнюю цифру введенного числа.

--- src/LectureTwo/TaskOne.java
+++ src/LectureTwo/TaskOne.java
@@ -1,7 +1,14 @@
 package LectureTwo;
 
+import java.util.Scanner;
+
 public class TaskOne {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        //возможно не самое лучшее решение, но мне показалось
+        //что для решения данной задачи - оптимально
+        //возможно - не прав
+        String s = in.nextLine();
+        System.out.println(s.charAt(s.length()-1));
     }
 }
