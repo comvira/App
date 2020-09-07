1e5d1c8f21702c96d3057474bfbe697e2fba9029
comvira <60033796+comvira@users.noreply.github.com>
2020-09-07 10:42:39 +0300

    Дан массив целых чисел: [15,10,51,-6,-5,3,-10,-34,0,32,56,-12,24,-52]… Переставить элементы массива в обратном порядке. Вывести результат в консоль

--- src/LectureTwo/TaskNine.java
+++ src/LectureTwo/TaskNine.java
@@ -1,11 +1,19 @@
 package LectureTwo;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class TaskNine {
     public static void main(String[] args) {
         int [] in = {15,10,51,-6,-5,3,-10,-34,0,32,56,-12,24,-52};
-        //тут не очень понятно в задании как выводить - как массив с [] или так ?
-        for (int i = in.length - 1; i > -1; i --) System.out.print(in[i] + " ");
+        int i = in.length;
+        int [] out = new int[i];
+        i = 0;
+        for (int j = in.length - 1; j >= 0; j--)
+        {
+            out[i] = in[j];
+            i++;
+        }
+        System.out.println(Arrays.toString(out));
     }
 }
