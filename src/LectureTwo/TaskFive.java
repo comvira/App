8b55807cb0360813a7661b75c29da0b81717455e
comvira <60033796+comvira@users.noreply.github.com>
2020-09-07 09:28:17 +0300

    Ввести три целых числа с консоли. Вывести на экран наименьшее из них.

--- src/LectureTwo/TaskFive.java
+++ src/LectureTwo/TaskFive.java
@@ -1,18 +1,17 @@
 package LectureTwo;
-
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class TaskFive {
     public static void main(String[] args) {
-        //тут мне показалось что, возможно к решению этой
-        //задачи можно так. возможно не прав и можно проще
         Scanner in = new Scanner(System.in);
-        int[] n = new int[3];
-        for (int i = 0; i < n.length; i++) {
-            n[i] = in.nextInt();
+        int min = (int) 1e9;
+        int n;
+        for (int i = 0; i < 3; i++) {
+            n = in.nextInt();
+            if (n < min) {
+                min = n;
+            }
         }
-        Arrays.sort(n);
-        System.out.println(n[0]);
+        System.out.println(min);
     }
 }
