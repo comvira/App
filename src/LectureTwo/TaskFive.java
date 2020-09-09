3ab01f6b6518f514e1136f21a53d867e9e037257
comvira <60033796+comvira@users.noreply.github.com>
2020-09-09 08:52:02 +0300

    Ввести три целых числа с консоли. Вывести на экран наименьшее из них.

--- src/LectureTwo/TaskFive.java
+++ src/LectureTwo/TaskFive.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class TaskFive {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int min = (int) 1e9;
+        int min = 2147483647;
         int n;
         for (int i = 0; i < 3; i++) {
             n = in.nextInt();
