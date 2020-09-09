3c18a4ff3cbb39ee6ed08930fbc4141f4557c62f
comvira <60033796+comvira@users.noreply.github.com>
2020-09-09 08:39:37 +0300

    Ввести целое число в консоли. Вывести его строкуописание вида «отрицательное четное число», «нулевое число», «положительное нечетное число» и т. д.

--- src/LectureTwo/TaskSix.java
+++ src/LectureTwo/TaskSix.java
@@ -2,7 +2,7 @@ package LectureTwo;
 
 import java.util.Scanner;
 
-public class TaskSex {
+public class TaskSix {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
