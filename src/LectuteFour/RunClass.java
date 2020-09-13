eafcab55c53d6d40254e58c611ea7e31a2df403e
comvira <60033796+comvira@users.noreply.github.com>
2020-09-13 13:57:31 +0300

    Person & Employee

--- src/LectuteFour/RunClass.java
+++ src/LectuteFour/RunClass.java
@@ -4,6 +4,7 @@ public class RunClass {
     public static void main(String[] args) {
         Employee employee = new Employee("A");
         Person person = new Employee("B");
+        System.out.println(employee.getName());
         System.out.println(person.getName());
     }
 }
