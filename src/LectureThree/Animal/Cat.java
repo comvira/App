07843bb32235c86e4e320ec42aa2111dfbf7dc38
comvira <60033796+comvira@users.noreply.github.com>
2020-09-20 16:44:51 +0300

    Класс Animal

--- src/LectureThree/Animal/Cat.java
+++ src/LectureThree/Animal/Cat.java
@@ -3,44 +3,45 @@ package LectureThree.Animal;
 public class Cat extends Animal{
     private String name = "Cat";
     private String eat;
-    private String location;
     private String sleep = "Нет";
 
     public Cat(String eat) {
         super(eat);
     }
 
+    public Cat(String eat, String name, String sleep) {
+        super(eat);
+        this.name = name;
+        this.sleep = sleep;
+    }
+
     public String getName() {
+
         return name;
     }
 
     public String getEat() {
+
         return eat;
     }
 
-    @Override
-    public String getLocation() {
-        return location;
-    }
-
     public String getSleep() {
+
         return sleep;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public void setEat(String eat) {
+
         this.eat = eat;
     }
 
-    @Override
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
     public void setSleep(String sleep) {
+
         this.sleep = sleep;
     }
 }
