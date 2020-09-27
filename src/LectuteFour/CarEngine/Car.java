6ed5eacbebd22c131484a291f556da4770d1f11c
comvira <60033796+comvira@users.noreply.github.com>
2020-09-27 09:38:47 +0300

    Car

--- src/LectuteFour/CarEngine/Car.java
+++ src/LectuteFour/CarEngine/Car.java
@@ -6,6 +6,36 @@ public abstract class Car extends Engine{
     public abstract String start();
     public abstract String stop();
     public abstract void turnRight();
+
+
+    @Override
+    public int getPower() {
+        return Engine.power;
+    }
+
+    @Override
+    public void setPower(int power) {
+        Engine.power = power;
+    }
+
+    @Override
+    public String getManufacturer() {
+        return Engine.manufacturer;
+    }
+
+    @Override
+    public void setManufacturer(String manufacturer) {
+        Engine.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        Car.model = model;
+    }
+
     public abstract void turnLeft();
     public abstract void printInfo();
 
