f6c20807cb5a885d382364a9f3b3101392d4b0ed
comvira <60033796+comvira@users.noreply.github.com>
2020-09-29 14:38:42 +0300

    Files

---
+++ src/Files/File2Read.java
@@ -0,0 +1,17 @@
+package Files;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class File2Read {
+    public static void main(String[] args) {
+        try {
+            File file = new File("C:\\Date\\test.txt");
+            Scanner sc = new Scanner(file);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("not file");
+        }
+    }
+}
