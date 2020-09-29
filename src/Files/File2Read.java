75d27bae1722e4df2b93894eb5665728938ece78
comvira <60033796+comvira@users.noreply.github.com>
2020-09-29 14:39:30 +0300

    Files

--- src/Files/File2Read.java
+++ src/Files/File2Read.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class File2Read {
     public static void main(String[] args) {
         try {
-            File file = new File("C:\\Date\\test.txt");
+            File file = new File("C:\\Data\\test.txt");
             Scanner sc = new Scanner(file);
         }
         catch (FileNotFoundException e) {
