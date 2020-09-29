ad105f05a86a1d4f944ee91ecdf9f760f569ac7c
comvira <60033796+comvira@users.noreply.github.com>
2020-09-29 16:10:50 +0300

    Files

--- src/Files/File3Create.java
+++ src/Files/File3Create.java
@@ -1,11 +1,22 @@
 package Files;
 
+import java.io.File;
 import java.util.Formatter;
+import java.util.Scanner;
 
 public class File3Create {
     public static void main(String[] args) {
         try {
             Formatter file = new Formatter("C:\\Data\\test.txt");
+            file.format("%s %s", "test1", "2 \r \n");
+            file.format("%s %s", "test2", "8");
+            file.close();
+            File f = new File("C:\\Data\\test.txt");
+            Scanner sc = new Scanner(f);
+            while (sc.hasNext()) {
+                System.out.println(sc.next());
+            }
+            sc.close();
         }
         catch (Exception e) {
             System.out.println("Error");
