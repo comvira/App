a02da854055e2017741246157fae730d7ba7a3b1
comvira <60033796+comvira@users.noreply.github.com>
2020-09-29 13:30:24 +0300

    HashSet

--- src/Hash/Hash2Set.java
+++ src/Hash/Hash2Set.java
@@ -1,6 +1,7 @@
 package Hash;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 public class Hash2Set {
     public static void main(String[] args) {
@@ -8,6 +9,7 @@ public class Hash2Set {
         s.add("A");
         s.add("B");
         s.add("C");
+        //new LinkedHashSet(s);// тут непонятка пока
         System.out.println(s);
         System.out.println(s.size());
     }
