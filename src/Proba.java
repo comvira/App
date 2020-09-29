bd86151a151df2886a0903514494e053f3e91e39
comvira <60033796+comvira@users.noreply.github.com>
2020-09-29 16:46:39 +0300

    Proba

---
+++ src/Proba.java
@@ -0,0 +1,9 @@
+public class Proba {
+    public static void main(String[] args) {
+        int f = 1, i = 2;
+        while (++i < 5) {
+            f*= i;
+        }
+        System.out.println(f);
+    }
+}
