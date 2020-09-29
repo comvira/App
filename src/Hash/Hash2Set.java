19ad572ef47a48f293f2328f2cbd8d08a5f9192d
comvira <60033796+comvira@users.noreply.github.com>
2020-09-29 12:53:31 +0300

    HashSet

---
+++ src/Hash/Hash2Set.java
@@ -0,0 +1,13 @@
+package Hash;
+
+import java.util.HashSet;
+
+public class Hash2Set {
+    public static void main(String[] args) {
+        HashSet<String> s = new HashSet<String>();
+        s.add("A");
+        s.add("B");
+        s.add("C");
+        System.out.println(s);
+    }
+}
