8deeaa6066e35e77c76bba3924bb6212fe53f61d
comvira <60033796+comvira@users.noreply.github.com>
2020-10-02 18:29:20 +0300

    Builder

---
+++ src/LectuteFive/Builder/BuilderRun.java
@@ -0,0 +1,10 @@
+package LectuteFive.Builder;
+
+public class BuilderRun {
+    public static void main(String[] args) {
+        CarBuilder  carBuilder = new CarBuilder();
+
+        Car car = carBuilder.withName("Ока").withEngine("V1").withSpeed(60).withWeight(100).build();
+        System.out.println(car);
+    }
+}
