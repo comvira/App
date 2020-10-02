a39ccfa2b1f0e805017364063838fc7af3951549
comvira <60033796+comvira@users.noreply.github.com>
2020-10-02 13:55:54 +0300

    Singleton

---
+++ src/LectuteFive/Singleton/SingletonRun.java
@@ -0,0 +1,9 @@
+package LectuteFive.Singleton;
+
+public class SingletonRun {
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        System.out.println(s1 == s2);
+    }
+}
