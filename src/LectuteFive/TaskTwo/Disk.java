b674a5691e57a2a7ea01ceda87f17396dec16231
comvira <60033796+comvira@users.noreply.github.com>
2020-10-04 11:00:40 +0300

    TaskTwo Computer

--- src/LectuteFive/TaskTwo/Disk.java
+++ src/LectuteFive/TaskTwo/Disk.java
@@ -1,4 +1,40 @@
 package LectuteFive.TaskTwo;
 
 public class Disk {
+    private String manufacture;
+    private int memorySize;
+    private String typeDisk;
+    private int formFactor;
+
+    public String getManufacture() {
+        return manufacture;
+    }
+
+    public void setManufacture(String manufacture) {
+        this.manufacture = manufacture;
+    }
+
+    public int getMemorySize() {
+        return memorySize;
+    }
+
+    public void setMemorySize(int memorySize) {
+        this.memorySize = memorySize;
+    }
+
+    public String getTypeDisk() {
+        return typeDisk;
+    }
+
+    public void setTypeDisk(String typeDisk) {
+        this.typeDisk = typeDisk;
+    }
+
+    public int getFormFactor() {
+        return formFactor;
+    }
+
+    public void setFormFactor(int formFactor) {
+        this.formFactor = formFactor;
+    }
 }
--- src/LectuteFive/TaskTwo/Processor.java
+++ src/LectuteFive/TaskTwo/Processor.java
@@ -1,4 +1,50 @@
 package LectuteFive.TaskTwo;
 
 public class Processor {
+
+    private String clockFrequency;
+    private int numCore;
+    private int memorySize;
+    private String name;
+    private String manufacture;
+
+    public String getClockFrequency() {
+        return clockFrequency;
+    }
+
+    public void setClockFrequency(String clockFrequency) {
+        this.clockFrequency = clockFrequency;
+    }
+
+    public int getNumCore() {
+        return numCore;
+    }
+
+    public void setNumCore(int numCore) {
+        this.numCore = numCore;
+    }
+
+    public int getMemorySize() {
+        return memorySize;
+    }
+
+    public void setMemorySize(int memorySize) {
+        this.memorySize = memorySize;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacture() {
+        return manufacture;
+    }
+
+    public void setManufacture(String manufacture) {
+        this.manufacture = manufacture;
+    }
 }
