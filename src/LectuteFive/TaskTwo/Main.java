83d14d42712a430cd3489250462a473ff434875a
comvira <60033796+comvira@users.noreply.github.com>
2020-10-04 13:43:39 +0300

    TaskTwo Computer

--- src/LectuteFive/TaskTwo/Main.java
+++ src/LectuteFive/TaskTwo/Main.java
@@ -1,17 +1,24 @@
 package LectuteFive.TaskTwo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String monitor;
+        ArrayList arr = new ArrayList();
 
         System.out.println("Введите иарку монитора:");
         monitor = sc.nextLine();
+        arr.add("Монитор: " + monitor + ", ");
 
         ComputerAssembler computerAssembler = new ComputerAssembler();
         computerAssembler.getCreateComp();
 
+
+        System.out.println(Arrays.toString(arr.toArray()));
     }
 }
