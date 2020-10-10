7ba4dd651852a72268110270f9c784a6ce5030f5
comvira <60033796+comvira@users.noreply.github.com>
2020-10-10 16:30:08 +0300

    Calendar

---
+++ src/CalendarRun.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar.getTime());
+        System.out.println(calendar.getClass().getCanonicalName());
+    }
+}
