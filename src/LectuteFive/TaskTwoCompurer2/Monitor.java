a8aff7c7cea6c65d1267f1aef3a571f0191803a2
comvira <60033796+comvira@users.noreply.github.com>
2020-10-14 14:55:06 +0300

    Computer2

---
+++ src/LectuteFive/TaskTwoCompurer2/Monitor.java
@@ -0,0 +1,13 @@
+package LectuteFive.TaskTwoCompurer2;
+
+public class Monitor extends Detail {
+    //private String manufacture;
+
+    public Monitor(String manufacture) {
+        super.manufacture = manufacture;
+    }
+
+    public String getManufacture() {
+        return manufacture;
+    }
+}
