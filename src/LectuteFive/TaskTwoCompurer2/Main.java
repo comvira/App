49cbab3ee6ca656e3f836d8390330ce89e92b312
comvira <60033796+comvira@users.noreply.github.com>
2020-10-15 06:44:03 +0300

    Computer2

--- src/LectuteFive/TaskTwoCompurer2/Main.java
+++ src/LectuteFive/TaskTwoCompurer2/Main.java
@@ -4,9 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Monitor monitor = new Monitor("Sony");
         Memory memory = new Memory("Toshiba",8,4,"DDR");
+
         System.out.println("ОЗУ:");
         memory.createComputer();
+
         System.out.println();
+
         System.out.println("Монитор:");
         monitor.createComputer();
 
