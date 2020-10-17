7a67a86ac1404c528fa254e5c2b41805648ef0b3
comvira <60033796+comvira@users.noreply.github.com>
2020-10-17 12:56:16 +0300

    Computer_Test

--- src/LectuteFive/Test/Main.java
+++ src/LectuteFive/Test/Main.java
@@ -3,7 +3,7 @@ package LectuteFive.Test;
 public class Main {
     public static void main(String[] args) {
         Monitor monitor = new Monitor("Philips");
-        System.out.println(monitor.getManufacture());
+        System.out.println("Монитор: " + monitor.getManufacture());
         monitor.createComputer();
     }
 }
