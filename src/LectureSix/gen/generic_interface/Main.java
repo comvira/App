685c59887efb88185828775a9c1493b146470550
comvira <60033796+comvira@users.noreply.github.com>
2020-10-18 14:55:48 +0300

    Generic

---
+++ src/LectureSix/gen/generic_interface/Main.java
@@ -0,0 +1,10 @@
+package LectureSix.gen.generic_interface;
+
+public class Main {
+    public static void main(String[] args) {
+        PairString strPair = new PairString("String1", "String2");
+
+        System.out.println(strPair.getVal1());
+        System.out.println(strPair.getVal2());
+    }
+}
