f0c83bfc69abe2f9d5841de7ec876281589a198b
comvira <60033796+comvira@users.noreply.github.com>
2020-10-24 13:09:48 +0300

    FileShool + Exception

--- src/FilesSchool/Saver.java
+++ src/FilesSchool/Saver.java
@@ -1,4 +1,20 @@
 package FilesSchool;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static java.io.File.*;
+
 public class Saver {
+    public static void main(String[] args) {
+        System.out.println(separatorChar);// win - \ Unix - /
+        System.out.println(pathSeparatorChar);// win - ; Unix - :
+        System.out.println(pathSeparator);
+        try(PrintWriter writer = new PrintWriter(new FileWriter("C:/Temp/afilatov/test.txt"))){
+            writer.println("test тест");
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
 }
