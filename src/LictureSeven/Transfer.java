974235993b35b94320d654d22acd7c13fcd6c3f0
comvira <60033796+comvira@users.noreply.github.com>
2020-10-27 06:33:04 +0300

    LectureSeven

--- src/LictureSeven/Transfer.java
+++ src/LictureSeven/Transfer.java
@@ -30,6 +30,8 @@ public class Transfer implements Operation {
                     }
                 }
             }
+
+
         } catch (IOException ex) {
             ex.printStackTrace();
         }
